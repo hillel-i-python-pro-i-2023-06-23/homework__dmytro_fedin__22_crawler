70dc4d3d076ebb9132592bd055cbee8a74582e57
dafedin <tm.i.sm.master@gmail.com>
2023-10-19 21:05:58 +0300

    Recursion exposed into main() to have search depth

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import logging
 import aiohttp
 import bs4
 from typing import TypeAlias
+from urllib.parse import urlparse
 from source.logger import get_custom_logger
 
 T_URL: TypeAlias = str
@@ -12,29 +13,35 @@ T_URLS_AS_SET: TypeAlias = set[T_URL]
 T_TEXT: TypeAlias = str
 
 
+def is_valid_url(url: T_URL) -> bool:
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
 async def get_urls_from_text(text: T_TEXT) -> T_URLS_AS_SET:
     soup = bs4.BeautifulSoup(markup=text, features="html.parser")
 
     urls = set()
     for link_element in soup.find_all("a"):
         url = link_element.get("href")
-        urls.add(url)
+
+        if is_valid_url(url):
+            urls.add(url)
 
     return set(urls)
 
 
-async def make_request(
-    url: T_URL,
-    session: aiohttp.ClientSession,
-    logger: logging.Logger,
-) -> T_TEXT:
+async def make_request(url: T_URL, session: aiohttp.ClientSession, logger: logging.Logger) -> T_TEXT:
     async with session.get(url) as response:
         logger.info(response.status)
         return await response.text()
 
 
 async def handle_url(url: T_URL, session: aiohttp.ClientSession) -> T_URLS:
-    logger = get_custom_logger(name=url)
+    logger = get_custom_logger(name=str(url))
 
     text = await make_request(url=url, session=session, logger=logger)
 
@@ -43,20 +50,25 @@ async def handle_url(url: T_URL, session: aiohttp.ClientSession) -> T_URLS:
     return list(urls_as_set)
 
 
-async def main():
-    urls = [
-        "https://example.com/",
-        "https://www.djangoproject.com/",
-    ]
+async def main(urls: T_URLS, depth: int, all_results: list = None):
+    if all_results is None:
+        all_results = []
 
-    async with aiohttp.ClientSession(
-        # If you need to ignore cookies.
-        # https://docs.aiohttp.org/en/stable/client_reference.html#aiohttp.DummyCookieJar
-        # cookie_jar=aiohttp.DummyCookieJar(),
-    ) as session:
+    if depth <= 0:
+        return all_results
+
+    async with aiohttp.ClientSession() as session:
         tasks = [handle_url(url=url, session=session) for url in urls]
 
         results = await asyncio.gather(*tasks)
 
-    for result in results:
-        print(result)
+        flattened_results = [item for sublist in results for item in sublist]
+
+        all_results.extend(flattened_results)
+
+        print(all_results)
+
+        await main(urls, depth - 1, all_results)
+
+    return all_results
+
--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import asyncio
 from app import main
-# from source.logger import write_log
+from source import get_url_depth, get_url_list
+
+urls = get_url_list()
+recursion_depth = get_url_depth()
 
 if __name__ == "__main__":
-
-    asyncio.run(main())
+    asyncio.run(main(urls, recursion_depth))
